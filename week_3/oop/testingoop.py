8d256706af06f2a3101a61548b4d7b90d3cf0bbb
christiankelechi <kezechristian@gmail.com>
2024-08-06 16:23:35 +0100

    working on oop

---
+++ week_3/oop/testingoop.py
@@ -0,0 +1,12 @@
+import sys
+import pathlib
+import os
+# path=pathlib.Path()
+# print(path)
+# base_dir=os.path.join(path,'oop')
+# sys.path.append(base_dir)
+import human_being as h
+
+obi=h.HumanBeing("eye","leg","hand","mouth")
+
+print(obi.mouth)
